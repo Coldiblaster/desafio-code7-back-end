4878bbbbcafb492c85a4f465bc12083518dd28bd
Coldiblaster <vbastazin@gmail.com>
2020-11-07 12:27:16 -0300

    Add route

--- src/routes/debts.routes.ts
+++ src/routes/debts.routes.ts
@@ -1,9 +1,32 @@
 import { Router } from 'express';
 
-const debtsRouter = Router();
+import DebtsRepository from '../repositories/DebtsRepository';
+import CreateDebtsService from '../services/CreateDebtService';
 
-debtsRouter.get('/', (request, response) => {
-  return response.json('teste');
+const debtsRouter = Router();
+const debtsRepository = new DebtsRepository();
+
+debtsRouter.post('/', (request, response) => {
+  const { idUser, debtReason, debtDate, value } = request.body;
+
+  const createDebt = new CreateDebtsService(debtsRepository);
+
+  const debt = createDebt.execute({
+    idUser,
+    debtReason,
+    debtDate,
+    value,
+  });
+
+  return response.json(debt);
+});
+
+debtsRouter.get('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const debt = debtsRepository.getDebt(Number(id));
+
+  return response.json(debt);
 });
 
 export default debtsRouter;
